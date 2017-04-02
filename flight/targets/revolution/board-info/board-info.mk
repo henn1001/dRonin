75c4513b055a9b0c7ef4c8ff934457a90c4c6c61
henn1001 <hendrik-schlee@web.de>
2017-04-02 19:07:08 +0200

    replace alarm led with buzzer

--- flight/targets/revolution/board-info/board-info.mk
+++ flight/targets/revolution/board-info/board-info.mk
@@ -1,6 +1,6 @@
 BOARD_TYPE          := 0x09
 BOARD_REVISION      := 0x03
-BOOTLOADER_VERSION  := 0x87
+BOOTLOADER_VERSION  := 0x88
 HW_TYPE             := 0x00
 
 CHIP                := STM32F405RGT
--- flight/targets/revolution/board-info/pios_board.h
+++ flight/targets/revolution/board-info/pios_board.h
@@ -88,8 +88,9 @@ TIM8  |           |           |           |
 //------------------------
 // PIOS_LED
 //------------------------
-#define PIOS_LED_HEARTBEAT	0
-#define PIOS_LED_ALARM		1
+#define PIOS_LED_HEARTBEAT			0
+#define PIOS_LED_ALARM				0
+#define PIOS_ANNUNCIATOR_BUZZER		1
 
 //------------------------
 // PIOS_WDG
